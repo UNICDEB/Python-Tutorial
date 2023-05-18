795047c3e2253b26ffff18ba8025eb2a8a83bcfe
DEBABRATA DOLOI <97942932+UNICDEB@users.noreply.github.com>
2023-05-18 12:44:32 +0530

    Add files via upload

---
+++ Dictionary_Code/Dict01.py
@@ -0,0 +1,22 @@
+#
+#Create and print a dictionary:
+
+thisdict = {
+  "Brand": "Honda",
+  "Model": "City",
+  "Year": 2000
+}
+# print(thisdict)
+
+# print(thisdict["Model"])
+# print(len(thisdict))
+
+# Get a list of the keys:
+# x = thisdict.keys()
+# print(x)
+
+# #Get a list of the values:
+x = thisdict.values()
+print(x)
+
+
