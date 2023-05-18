e0c9d4f6d943b7ac3741023f3d45554ba56bfcbd
DEBABRATA DOLOI <97942932+UNICDEB@users.noreply.github.com>
2023-05-18 12:46:25 +0530

    Add files via upload

---
+++ Matrix/matrixAdd(np).py
@@ -0,0 +1,63 @@
+# # 
+# Program to add two matrices using nested loop
+# import numpy as np
+
+# # x= np.array([[]])
+ 
+# X = [[1,2,3],
+#     [4 ,5,6],
+#     [7 ,8,9]]
+ 
+# Y = [[9,8,7],
+#     [6,5,4],
+#     [3,2,1]]
+ 
+ 
+# result = [[0,0,0],
+#         [0,0,0],
+#         [0,0,0]]
+ 
+# # iterate through rows
+# for i in range(len(X)):  
+# # iterate through columns
+#     for j in range(len(X[0])):
+#         result[i][j] = X[i][j] + Y[i][j]
+ 
+# for r in result:
+#     print(r)
+    
+###################
+#
+# Program to add two matrices
+# using list comprehension
+  
+# X = [[1,2,3],
+#     [4 ,5,6],
+#     [7 ,8,9]]
+  
+# Y = [[9,8,7],
+#     [6,5,4],
+#     [3,2,1]]
+ 
+# result = [[X[i][j] + Y[i][j]  for j in range
+# (len(X[0]))] for i in range(len(X))]
+  
+# for r in result:
+#     print(r)
+    
+#################
+#
+# Program to add two matrices
+# using zip()
+  
+X = [[1,2,3],
+    [4 ,5,6],
+    [7 ,8,9]]
+  
+Y = [[9,8,7],
+    [6,5,4],
+    [3,2,1]]
+ 
+result = [map(sum, zip(*t)) for t in zip(X, Y)]
+  
+print(result)
